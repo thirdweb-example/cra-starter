ea9055aa6d469efdafc55a57f0387fbca6bdc003
Jarrod Watts <jarrodwatts16@gmail.com>
2022-09-05 23:07:59 +1000

    Fix with contributor suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import { ConnectWallet } from "@thirdweb-dev/react";
 
 function App() {
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
-  return (
-    <div>
-      {address ? (
-        <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
-      )}
-    </div>
-  );
+  return <ConnectWallet />;
 }
 
 export default App;
