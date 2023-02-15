fc46de87ab22589012d514ab746448b1377638f5
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-15 14:37:16 -0800

    Revert "prepare for anyevm"
    This reverts commit 37d9cd9bd976c6c960e1e70a82b447aefd0be772.

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider desiredChainId={activeChainId}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
